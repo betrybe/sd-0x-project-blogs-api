591d9a4e1cdc26348b73d4dcb1061d86291a3f68
nato-re <alvares.renato@hotmail.com>
2020-12-08 10:30:45 -0300

    Adiciona nova versao da action de lint em main.yml e eslint 6.8.0

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const express = require('express');
 
 const app = express();
 
+const test = () =>  1
+const test1 = () => 1
+
+
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
 // não remova esse endpoint, e para o avaliador funcionar
+app.get('/', (request, response) => {
+  response.send();
+});
+
 app.get('/', (request, response) => {
   response.send();
 });
