c505a71eaf41d2d2d55dc71700e5f6af1acaabee
nato-re <alvares.renato@hotmail.com>
2020-12-10 12:01:04 -0300

    readme no padrao

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 const express = require('express');
 
 const app = express();
-const testt = [122, 3];
-const test = () => testt;
-const test1 = () => testt;
-
-console.log(test1 + test);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
 // não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (request, response) => {
-  response.send();
-});
-
 app.get('/', (request, response) => {
   response.send();
 });
